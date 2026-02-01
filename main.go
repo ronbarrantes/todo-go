3c8c9ecea4e51b31611794080e3129992e3e203e
Ron Barrantes <ronbarrantes@gmail.com>
2026-01-31 20:23:28 -0500

    Add the generate function

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"log"
 	"time"
 )
+
+func generateId() string {
+	var id [3]byte
+	_, err := rand.Read(id[:])
+	if err != nil {
+		log.Fatal("Error reading random bytes", err)
+	}
+	return fmt.Sprintf("%x", id)
+}
+
+type ToDo struct {
+	ID         string
+	Text       string
+	IsComplete bool
+	Date       time.Time
+}
+
+func (t *ToDo) Create() {
+}
+
+func (t *ToDo) Read() {
+}
+
+func (t *ToDo) Update() {
+}
+
+func (t *ToDo) Delete() {
+}
 
 // going to make a todo
 // FULL CRUD
@@ -22,5 +52,8 @@ func main() {
 		fmt.Printf("This program took %v to complete\n", duration)
 	}()
 
+	val := generateId()
+
+	fmt.Printf("Random Value: %v\n", val)
 	fmt.Printf("This is my To Do program\n")
 }
