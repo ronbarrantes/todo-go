3d7b7094b0a23a5d1821653634baa4b029e7ba18
ronbarrantes <ronbarrantes@gmail.com>
2026-02-12 22:11:09 -0500

    Add db crud

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"slices"
-	s "strings"
 	"time"
 
 	"gorm.io/driver/sqlite"
@@ -18,9 +14,12 @@ import (
 )
 
 type ToDo struct {
-	gorm.Model
+	ID          string `gorm:"primaryKey"`
 	Text        string `gorm:"text"`
 	IsCompleted bool   `gorm:"is_completed"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
 
 type DBContext struct {
@@ -80,27 +79,30 @@ func printToDo(t *ToDo) {
 	fmt.Printf("- [%s] (%s) %s\n", completed, t.ID, t.Text)
 }
 
-func writeJSON(t []*ToDo) error {
-	jsonBlob, err := json.MarshalIndent(t, "", " ")
-	if err != nil {
+func (ctx *DBContext) Create(t *ToDo) error {
+	id := generateId()
+	item := &ToDo{
+		ID:          id,
+		Text:        t.Text,
+		IsCompleted: false,
+	}
+
+	if err := ctx.db.Create(item).Error; err != nil {
 		return err
 	}
 
-	path, err := getUserDataPath()
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(path, jsonBlob, 0644)
+	fmt.Printf("To-do %s created\n", id)
+	return nil
 }
 
 func (ctx *DBContext) Read() error {
 	var todos []*ToDo
-	if err := ctx.db.Find(todos).Error; err != nil {
+	if err := ctx.db.Find(&todos).Error; err != nil {
 		return err
 	}
 
 	if len(todos) == 0 {
-		return fmt.Errorf("there are no to-dos")
+		return fmt.Errorf("There are no to-dos")
 	}
 
 	for _, todo := range todos {
@@ -110,143 +112,84 @@ func (ctx *DBContext) Read() error {
 	return nil
 }
 
-func readJSON() ([]*ToDo, error) {
-	path, err := getUserDataPath()
-	if err != nil {
-		return nil, err
-	}
-
-	todos := []*ToDo{}
-	data, err := os.ReadFile(path)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return todos, nil
-		}
-
-		return nil, err
-	}
-
-	if len(data) == 0 {
-		return todos, nil
-	}
-
-	if err = json.Unmarshal(data, &todos); err != nil {
-		return nil, err
-	}
-
-	return todos, nil
-}
-
-func updateStore(fn func([]*ToDo) ([]*ToDo, error)) error {
-	jData, err := readJSON()
-	if err != nil {
+func (ctx *DBContext) Update(td *ToDo) error {
+	if err := ctx.db.Model(&td).Update("Text", &td.Text).Error; err != nil {
+		// fmt.Errorf("to do %s not found", t.ID)
 		return err
 	}
-
-	toReadAndWrite, err := fn(jData)
-	if err != nil {
-		return err
-	}
-
-	err = writeJSON(toReadAndWrite)
-	if err != nil {
-		return err
-	}
-
+	fmt.Printf("To-do %s updated", td.ID)
 	return nil
 }
 
-func (t *ToDo) Create() error {
-	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
-		item := &ToDo{
-			Text:        t.Text,
-			IsCompleted: false,
-		}
-
-		td = append(td, item)
-
-		printToDo(item)
-		return td, nil
-	})
-}
-
-func (t *ToDo) Read() error {
-	jData, err := readJSON()
-	if err != nil {
+func (ctx *DBContext) Delete(td *ToDo) error {
+	if err := ctx.db.Delete(&td).Error; err != nil {
+		// fmt.Errorf("to do %s not found", t.ID)
 		return err
 	}
-
-	if len(jData) == 0 {
-		return fmt.Errorf("there are no to dos")
-	}
-
-	for _, todo := range jData {
-		printToDo(todo)
-	}
-
+	fmt.Printf("To-do %s deleted", td.ID)
 	return nil
 }
 
-func (t *ToDo) Update() error {
-	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
-		if len(td) == 0 {
-			fmt.Println("nothing to do!")
-			return []*ToDo{}, nil
-		}
+// func (t *ToDo) Update() error {
+// 	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
+// 		if len(td) == 0 {
+// 			fmt.Println("nothing to do!")
+// 			return []*ToDo{}, nil
+// 		}
 
-		todo, err := t.FindItem(td)
-		if err != nil {
-			return nil, err
-		}
+// 		todo, err := t.FindItem(td)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		todo.Text = t.Text
-		return td, nil
-	})
-}
+// 		todo.Text = t.Text
+// 		return td, nil
+// 	})
+// }
 
-func (t *ToDo) FindItem(td []*ToDo) (*ToDo, error) {
-	for _, todo := range td {
-		if len(t.ID) >= 4 && s.HasPrefix(todo.ID, t.ID) {
-			return todo, nil
-		}
-	}
+// func (t *ToDo) FindItem(td []*ToDo) (*ToDo, error) {
+// 	for _, todo := range td {
+// 		if len(t.ID) >= 4 && s.HasPrefix(todo.ID, t.ID) {
+// 			return todo, nil
+// 		}
+// 	}
 
-	return nil, fmt.Errorf("to do %s not found", t.ID)
-}
+// 	return nil, fmt.Errorf("to do %s not found", t.ID)
+// }
 
-func (t *ToDo) ToggleTodo() error {
-	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
-		if len(td) == 0 {
-			fmt.Println("nothing to do!")
-			return []*ToDo{}, nil
-		}
+// func (t *ToDo) ToggleTodo() error {
+// 	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
+// 		if len(td) == 0 {
+// 			fmt.Println("nothing to do!")
+// 			return []*ToDo{}, nil
+// 		}
 
-		todo, err := t.FindItem(td)
-		if err != nil {
-			return nil, err
-		}
+// 		todo, err := t.FindItem(td)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		todo.IsCompleted = !todo.IsCompleted
-		return td, nil
-	})
-}
+// 		todo.IsCompleted = !todo.IsCompleted
+// 		return td, nil
+// 	})
+// }
 
-func (t *ToDo) Delete() error {
-	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
-		found, err := t.FindItem(td)
-		if err != nil {
-			return nil, err
-		}
+// func (t *ToDo) Delete() error {
+// 	return updateStore(func(td []*ToDo) ([]*ToDo, error) {
+// 		found, err := t.FindItem(td)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		for i, todo := range td {
-			if todo.ID == found.ID {
-				return slices.Delete(td, i, i+1), nil
-			}
-		}
+// 		for i, todo := range td {
+// 			if todo.ID == found.ID {
+// 				return slices.Delete(td, i, i+1), nil
+// 			}
+// 		}
 
-		return nil, fmt.Errorf("to do with id %s not found", t.ID)
-	})
-}
+// 		return nil, fmt.Errorf("to do with id %s not found", t.ID)
+// 	})
+// }
 
 func main() {
 	s := time.Now()
@@ -269,8 +212,8 @@ func main() {
 	// findFlag := flag.String("f", "", "Find a to todo")
 	updateFlag := flag.String("u", "", "Update a to do")
 	textFlag := flag.String("t", "", "Update the text of a to do")
-	toggleCompleteFlag := flag.String("x", "", "Update completed state")
-	deleteFlag := flag.String("d", "", "Delete to do")
+	// toggleCompleteFlag := flag.String("x", "", "Update completed state")
+	// deleteFlag := flag.String("d", "", "Delete to do")
 	helpFlag := flag.Bool("h", false, "Show help")
 
 	flag.Parse()
@@ -284,7 +227,7 @@ func main() {
 
 	if len(os.Args) == 1 {
 		fmt.Println("Listing todos by default...")
-		err := todo.Read()
+		err := dbCtx.Read()
 		if err != nil {
 			fmt.Printf("error: %v\n", err)
 		}
@@ -294,14 +237,14 @@ func main() {
 	switch {
 	case *readFlag:
 		fmt.Println("Reading ....")
-		err := todo.Read()
+		err := dbCtx.Read()
 		if err != nil {
 			fmt.Printf("error: %v\n", err)
 		}
 
 	case *createFlag != "":
 		todo.Text = *createFlag
-		err := todo.Create()
+		err := dbCtx.Create(&todo)
 		if err != nil {
 			fmt.Printf("error: %v\n", err)
 		}
@@ -314,25 +257,25 @@ func main() {
 
 		todo.ID = *updateFlag
 		todo.Text = *textFlag
-		err := todo.Update()
+		err := dbCtx.Update(&todo)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "error: %v\n", err)
 			os.Exit(1)
 		}
 
-	case *toggleCompleteFlag != "":
-		todo.ID = *toggleCompleteFlag
-		err := todo.ToggleTodo()
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
+	// case *toggleCompleteFlag != "":
+	// 	todo.ID = *toggleCompleteFlag
+	// 	err := todo.ToggleTodo()
+	// 	if err != nil {
+	// 		fmt.Printf("error: %v\n", err)
+	// 	}
 
-	case *deleteFlag != "":
-		todo.ID = *deleteFlag
-		err := todo.Delete()
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
+	// case *deleteFlag != "":
+	// 	todo.ID = *deleteFlag
+	// 	err := todo.Delete()
+	// 	if err != nil {
+	// 		fmt.Printf("error: %v\n", err)
+	// 	}
 
 	default:
 		fmt.Println("Unknown flag or no action")
