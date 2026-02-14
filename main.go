eb813aff34bd877accc26f400511e00c133b9154
ronbarrantes <ronbarrantes@gmail.com>
2026-02-13 19:26:34 -0500

    Get the DB out

--- main.go
+++ main.go
@@ -1,184 +1,24 @@
 package main
 
 import (
-	"crypto/rand"
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 	"time"
 
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	dbstore "ronb.co/todo-go/dbstore"
 )
-
-type ToDo struct {
-	ID          string `gorm:"primaryKey"`
-	Text        string `gorm:"text"`
-	IsCompleted bool   `gorm:"is_completed"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
-}
-
-type Store struct {
-	db *gorm.DB
-}
-
-func (store *Store) GetDatabase() error {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return err
-	}
-
-	appDir := filepath.Join(home, ".local", "share", "todo-go")
-	if err := os.MkdirAll(appDir, 0700); err != nil {
-		return err
-	}
-
-	gormPath := filepath.Join(appDir, "gorm.db")
-
-	db, err := gorm.Open(sqlite.Open(gormPath), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-
-	store.db = db
-	return nil
-}
-
-func generateId() (string, error) {
-	var id [6]byte
-	_, err := rand.Read(id[:])
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%x", id), nil
-}
-
-// CREATE A FIND FUNCTION FOR SHORTENED IDS
-func printToDo(t *ToDo) {
-	completed := " "
-	if t.IsCompleted {
-		completed = "x"
-	}
-
-	fmt.Printf("- [%s] (%s) %s\n", completed, t.ID, t.Text)
-}
-
-func (store *Store) Create(t string) error {
-	id, err := generateId()
-	if err != nil {
-		return err
-	}
-
-	item := &ToDo{
-		ID:          id,
-		Text:        t,
-		IsCompleted: false,
-	}
-
-	if err := store.db.Create(item).Error; err != nil {
-		fmt.Printf("To-do %s not created\n", id)
-		return err
-	}
-
-	fmt.Printf("To-do %s created\n", id)
-	return nil
-}
-
-func (store *Store) Read() error {
-	var todos []*ToDo
-	if err := store.db.Find(&todos).Error; err != nil {
-		return err
-	}
-
-	if len(todos) == 0 {
-		return fmt.Errorf("There are no to-dos")
-	}
-
-	for _, todo := range todos {
-		printToDo(todo)
-	}
-
-	return nil
-}
-
-func (store *Store) FindShortId(prefix string) (*ToDo, error) {
-	var todos []ToDo
-
-	if len(prefix) < 3 {
-		return nil, fmt.Errorf("Prefix needs to be longer than 3 characters")
-	}
-
-	if err := store.db.Where("id LIKE ?", prefix+"%").Find(&todos).Error; err != nil {
-		return nil, err
-	}
-
-	switch len(todos) {
-	case 0:
-		return nil, fmt.Errorf("No to-do found")
-
-	case 1:
-		return &todos[0], nil
-
-	default:
-		return nil, fmt.Errorf("Too ambiguous")
-	}
-}
-
-func (store *Store) Update(td *ToDo) error {
-	todo, err := store.FindShortId(td.ID)
-	if err != nil {
-		return err
-	}
-
-	if err := store.db.Model(todo).Update("Text", td.Text).Error; err != nil {
-		fmt.Printf("To-do %s not updated\n", td.ID)
-		return err
-	}
-
-	fmt.Printf("To-do %s updated\n", td.ID)
-	return nil
-}
-
-func (store *Store) Delete(id string) error {
-	todo, err := store.FindShortId(id)
-	if err != nil {
-		return err
-	}
-	if err := store.db.Delete(todo).Error; err != nil {
-		fmt.Printf("To-do %s not deleted\n", id)
-		return err
-	}
-	fmt.Printf("To-do %s deleted\n", id)
-	return nil
-}
-
-func (store *Store) ToggleTodo(id string) error {
-	todo, err := store.FindShortId(id)
-	if err != nil {
-		return err
-	}
-	completed := !todo.IsCompleted
-	if err := store.db.Model(todo).Update("is_completed", completed).Error; err != nil {
-		return err
-	}
-
-	fmt.Printf("To-do %s toggled\n", id)
-	return nil
-}
 
 func main() {
 	s := time.Now()
-	var store Store
+	var store dbstore.Store
 	err := store.GetDatabase()
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		os.Exit(1)
 	}
 
-	store.db.AutoMigrate(&ToDo{})
+	store.DB.AutoMigrate(&dbstore.ToDo{})
 
 	defer func() {
 		duration := time.Since(s)
@@ -196,7 +36,7 @@ func main() {
 
 	flag.Parse()
 
-	todo := ToDo{}
+	todo := dbstore.ToDo{}
 
 	if *helpFlag {
 		flag.Usage()
